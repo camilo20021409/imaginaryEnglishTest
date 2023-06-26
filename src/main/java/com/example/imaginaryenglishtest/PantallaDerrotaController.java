26ba546353c1ce271d24a195bfefaabfdd8ac4c8
John Arias <johnelconsultor@gmail.com>
2023-06-25 21:44:25 -0500

    Pantalla derrota lleva a inicio
    Agregué el método a al controller de la pantalla derrota que carga la pantalla  de inicio al darle "jugar de nuevo"

--- src/main/java/com/example/imaginaryenglishtest/PantallaDerrotaController.java
+++ src/main/java/com/example/imaginaryenglishtest/PantallaDerrotaController.java
@@ -1,4 +1,31 @@
 package com.example.imaginaryenglishtest;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class PantallaDerrotaController {
+    @FXML
+    Button botonSigueIntentando;
+
+    @FXML
+    private void cargarPantallaInicio() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/Principal.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) botonSigueIntentando.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+
+
 }
