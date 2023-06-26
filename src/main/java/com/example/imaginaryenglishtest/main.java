f62c76f4806a683565789ba04a06eb74850bf066
Andres Rueda <camilo20021409@gmail.com>
2023-06-26 16:16:17 -0500

    derrota

---
+++ src/main/java/com/example/imaginaryenglishtest/main.java
@@ -0,0 +1,8 @@
+package com.example.imaginaryenglishtest;
+
+import com.example.imaginaryenglishtest.ImaginaryEnglishApplication;
+public class main {
+    public static void main(String[] args) {
+        ImaginaryEnglishApplication.main(args );
+    }
+}
--- src/main/java/main.java
+++
@@ -1,6 +0,0 @@
-import com.example.imaginaryenglishtest.ImaginaryEnglishApplication;
-public class main {
-    public static void main(String[] args) {
-        ImaginaryEnglishApplication.main(args );
-    }
-}
